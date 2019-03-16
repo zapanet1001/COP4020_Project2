220380e2d35add765eaa9a963ab1a7c0d5147ea9
zapanet1001 <ipolo1969@ufl.edu>
2019-03-16 01:26:12 -0400

    Statement Nodes Updated

--- javastuff.java
+++ javastuff.java
@@ -70,20 +70,48 @@ public class NotExpression extends Expression{}
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~STATEMENT NODES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-public class Block extends Statement{}
+public class Block extends Statement{
+    List<Statement> statements;
+}
 
-public class VarDecl extends Statement{}
+public class VarDecl extends Statement{
+    String var;     //left
+    Expression e;   //right
+}
 
-public class Expression extends Statement{}
+public class Expression extends Statement{
+    Expression expressionType;
+}
 
-public class IfElse extends Statement{}
+public class IfElse extends Statement{
+    Expression condition;
+    Statement then;
+    Statement else_body;
+}
 
-public class WhileLoop extends Statement{}
+public class WhileLoop extends Statement{
+    Expression condition;
+    Statement body;
+}
 
-public class ForLoop extends Statement{}
+public class ForLoop extends Statement{
+    Expression initiateVar;
+    Expression condition;
+    Block block;
+    Expression updateCounter;
+}
 
-public class FuncDecl extends Statement{}
+public class FuncDecl extends Statement{
+    String functionName;
+    List<Expression> parameters; 
+    Block block;
+}
 
-public class Function extends Statement{}
+public class FunctionCall extends Statement{
+    String functionName;
+    List<String> arguments;
+}
 
-public class Print extends Statement{}
+public class Print extends Statement{
+    List<Statement> statements;
+}
